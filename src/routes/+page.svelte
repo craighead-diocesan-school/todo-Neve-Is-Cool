d772e1ce490e85290f8d3e2856ece27a49118916
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-06-12 09:43:00 +1200

    Save and load not working. Sorting it out.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,22 +8,31 @@
   function removeTask(index) {
     tasks = [...tasks.slice(0, index), ...tasks.slice(index + 1)]
   }
+  function saveTasks() {
+    localStorage.todos = JSON.stringify(tasks)
+  }
+  function loadTasks() {
+    tasks = JSON.parse(localStorage.todos)
+  }
 </script>
 
 <Header />
 
 <main class="content section">
   <button on:click={addTask}>📝 Add</button>
+  <button on:click={saveTasks}>💾 Save</button>
+  <button on:click={loadTasks}>📡 Load</button>
+
   {#each tasks as task, index}
     <div class="task">
       <input bind:value={tasks[index]} />
+      <button
+        on:click={() => {
+          removeTask(index)
+        }}>🗑</button
+      >
     </div>
   {/each}
-  <button
-    on:click={() => {
-      removeTask(index)
-    }}>🗑</button
-  >
 </main>
 
 <footer class="footer">
@@ -33,5 +42,15 @@
 <style>
   .task {
     display: block;
+    margin: 0.5rem;
+  }
+
+  input {
+    padding: 0.5rem;
+  }
+
+  button {
+    border: #000 solid 1px;
+    padding: 0.5rem;
   }
 </style>
